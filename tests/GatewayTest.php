0e9cfcf57527bd319bd095a2a8860cc8be24dae1
邝嘉业 <kuangjy@qq.com>
2018-11-27 20:34:29 +0800

    完善支付完成的测试用例

--- tests/GatewayTest.php
+++ tests/GatewayTest.php
@@ -4,7 +4,6 @@ namespace Omnipay\QPay\Tests;
 
 use Omnipay\Omnipay;
 use Omnipay\QPay\Gateway;
-use Omnipay\QPay\Helper;
 use Omnipay\QPay\Message\CompletePurchaseResponse;
 use Omnipay\QPay\Message\QueryOrderResponse;
 use Omnipay\Tests\GatewayTestCase;
@@ -23,20 +22,27 @@ class GatewayTest extends GatewayTestCase
     {
         parent::setUp();
         $this->gateway = Omnipay::create('QPay');
-        $this->gateway->setMchId('mchid');
-        $this->gateway->setApiKey('apikey');
+        $this->gateway->setMchId('12345678');
+        $this->gateway->setApiKey('1234567890abcdef1234567890abcdef');
     }
 
     public function testCompletePurchase()
     {
-        $options = array(
-            'request_params' => array(
-                'appid' => '123456',
-                'mch_id' => '789456',
-                'trade_state' => 'Success',
-            ),
-        );
-        $options['request_params']['sign'] = Helper::sign($options['request_params'], $this->gateway->getApiKey());
+        $options = [
+            'request_params' => '<xml><appid><![CDATA[12345678]]></appid>
+    <bank_type><![CDATA[BALANCE]]></bank_type>
+    <cash_fee><![CDATA[1]]></cash_fee>
+    <fee_type><![CDATA[CNY]]></fee_type>
+    <mch_id><![CDATA[12345678]]></mch_id>
+    <nonce_str><![CDATA[7b14db232445d79c5c86d22bbd8898d3]]></nonce_str>
+    <out_trade_no><![CDATA[20161025_qpay_unified_order_A]]></out_trade_no>
+    <sign><![CDATA[5F30A60C8F27FE28B5218B0DC430B8D9]]></sign>
+    <time_end><![CDATA[20161025094946]]></time_end>
+    <total_fee><![CDATA[1]]></total_fee>
+    <trade_state><![CDATA[SUCCESS]]></trade_state>
+    <trade_type><![CDATA[NATIVE]]></trade_type>
+    <transaction_id><![CDATA[1900000109471610251307259064]]></transaction_id></xml>'
+        ];
 
         /**
          * @var CompletePurchaseResponse $response

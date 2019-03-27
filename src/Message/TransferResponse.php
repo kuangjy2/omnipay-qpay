b389a68691f2f58fa41e9f6bc7acccc047178097
liu guan <qfz9527@hotmail.com>
2019-03-27 16:31:02 +0800

    修复现金红包 发送成功状态判断 (#4)

---
+++ src/Message/TransferResponse.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Omnipay\QPay\Message;
+
+/**
+ * Class TransferResponse
+ *
+ * @package Omnipay\QPay\Message
+ * @link    https://qpay.qq.com/buss/wiki/206/1215
+ */
+class TransferResponse extends BaseAbstractResponse
+{
+}
